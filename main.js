8d196e87131fa846161f09f9c13c872fcfc6ca7a
Arjunroy99881122-coder <arjunroy99881122@gmail.com>
2026-03-12 11:51:05 +0600

    javaScriptproject

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-const form = Document.t.getElementById("username");
-const errorM = document.querySelector(.error);
-
-form.addEventListener("submit",(e) =>{
-  e.preventDefault(};
+const tittle= document.getElementById("tittle");
+const description= document.getElementById("desc");
+const changeBtn= document.getElementById("change-button");
+changeBtn.addEventListener("click",function(){
+  tittle.textContent = "Amra prothom barer moto Event listen kora sikhlam";
+  description.textContent = "You are seeing this because You Have Clicked The Button";
   
-  if (username.value ===""){
-    errorM.textcontent =username.nodeValue
-  }
+  
+  console.log("button clicked");
+  
+  
+});
 
+const themeButton= document.querySelector("#theme-button")
+themeButton.addEventListener("click",function(){
+  
+  document.body.classList.toggle("dark");
+  
+  });
